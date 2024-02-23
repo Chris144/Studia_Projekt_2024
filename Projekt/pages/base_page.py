3a488fc78a809a88f431a17c249ed5d4344db765
Chris144 <krzysztofktly@gmail.com>
2024-02-23 12:19:18 +0100

    Create base page for tests

--- Projekt/pages/base_page.py
+++ Projekt/pages/base_page.py
@@ -1,28 +1,8 @@
 
-import unittest
-from datetime import time, datetime
+class BasePage:
+    """
+    Base class for each page
+    """
 
-from selenium import webdriver
-from PIL import ImageGrab
-
-"""
-Base test for each page
-""" 
-
-
-class BaseTest(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.maximize_window()
-        self.driver.get("https://skleptest.pl/")
-        self.driver.implicitly_wait(10)
-
-    def screenshot(self):
-        self.screenshot = ImageGrab.grab()
-        datetime.now()
-        screenDatetime = datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
-        self.driver.save_screenshot(f"../screenshot_from_tests/account_screen/screenshot-{screenDatetime}.png")
-        self.screenshot.close()
-
-    def tearDown(self):
-        self.driver.quit()
+    def __init__(self, driver):
+        self.driver = driver
