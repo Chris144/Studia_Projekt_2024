f96c6fca5224b8ba981df3ba15e0301d95117e5a
Chris144 <krzysztofktly@gmail.com>
2024-03-06 11:56:43 +0100

    Add login locators

--- Projekt/utils/locators.py
+++ Projekt/utils/locators.py
@@ -9,4 +9,8 @@ class AccountLocators:
     REGISTER_BUTTON = (By.XPATH, '//input[@name="register"]')
     REGISTER_OUT_CLICK = (By.XPATH, '//label[@for="reg_password"]')
     STRONG_OF_PASSWORD = (By.XPATH, '//div[@aria-live="polite"]')
+    LOGIN_EMAIL = (By.ID, 'username')
+    LOGIN_PASSWORD = (By.ID, 'password')
+    LOGIN_BUTTON = (By.CSS_SELECTOR, 'input[value=Login]')
+    LOGIN_REMEMBER_ME = (By.ID, 'rememberme')
 
